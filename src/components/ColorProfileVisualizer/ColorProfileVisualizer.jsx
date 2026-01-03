5464765bd5c49fc0eacfe45b4f7fdb37f880a6ce
Patryk Kowalski <patrykkowalski0617@gmail.com>
2026-01-03 12:56:57 +0100

    visul refactor

--- src/components/ColorProfileVisualizer/ColorProfileVisualizer.jsx
+++ src/components/ColorProfileVisualizer/ColorProfileVisualizer.jsx
@@ -4,11 +4,17 @@ import { useStore } from "../../store/useStore";
 import ProfileRow from "./ProfileRow";
 import { useSoundEngine } from "./hooks/useSoundEngine";
 import * as S from "./parts";
+import useDescription from "./hooks/useDescription";
 
 const ColorProfileVisualizer = () => {
   const store = useStore();
   const engine = useSoundEngine();
   const [activeChordType, setActiveChordType] = useState(null);
+  const description = useDescription(
+    majorRootName,
+    minorRootName,
+    activeShapeName
+  );
 
   if (!store.activeShape?.colorProfile || !store.activeMusicContext)
     return null;
@@ -18,7 +24,6 @@ const ColorProfileVisualizer = () => {
     activeMusicContext,
     getNoteNameByOffset,
     getActiveShapeName,
-    tuneKey,
     getContextNotes,
     getShapeNotes,
   } = store;
@@ -42,61 +47,6 @@ const ColorProfileVisualizer = () => {
       type === "major" ? contextNotes.majorRoot : contextNotes.minorRoot;
     if (notes) notes.forEach((n) => engine.playNote(n, 4, true));
   };
-
-  const key = tuneKey.label;
-  const id = activeMusicContext.id;
-  const descriptions = {
-    tonic: (
-      <>
-        In the key of <span>{key}</span>, a sense of resolution may be provided
-        by the <span>{majorRootName}&nbsp;Major</span> or{" "}
-        <span>{minorRootName}&nbsp;Minor</span> chords.{" "}
-        <span>{activeShapeName} Set</span> will expose the following colors of
-        each:
-      </>
-    ),
-
-    subdominant: (
-      <>
-        In the key of <span>{key}</span>, a sense of motion may be provided by
-        the <span>{majorRootName}&nbsp;Major</span> or{" "}
-        <span>{minorRootName}&nbsp;Minor</span> chords.{" "}
-        <span>{activeShapeName} Set</span> will expose the following colors of
-        each:
-      </>
-    ),
-
-    dominant: (
-      <>
-        In the key of <span>{key}</span>, a sense of tension to{" "}
-        <span>{key.split("/")[0]} Major</span> chord may be provided by the{" "}
-        <span>{majorRootName}&nbsp;Major</span> chord.{" "}
-        <span>{activeShapeName}</span> Set will expose the following colors of
-        it:
-      </>
-    ),
-
-    phDominant: (
-      <>
-        In the key of <span>{key}</span>, a sense of tension to{" "}
-        <span>{key.split("/")[1]?.replace("m", "")} Minor</span> chord may be
-        provided by the <span>{majorRootName}&nbsp;Major</span> chord.{" "}
-        <span>{activeShapeName} Set</span> will expose the following colors of
-        it:
-      </>
-    ),
-  };
-
-  const description =
-    id === "tonics"
-      ? descriptions.tonic
-      : id === "subdominants"
-      ? descriptions.subdominant
-      : id === "dominant"
-      ? descriptions.dominant
-      : id === "dominant-ph"
-      ? descriptions.phDominant
-      : "";
 
   return (
     <>
---
+++ src/components/ColorProfileVisualizer/hooks/useDescription.js
@@ -0,0 +1,65 @@
+import { useStore } from "../../../store/useStore";
+
+const useDescription = (majorRootName, minorRootName, activeShapeName) => {
+  const { activeMusicContext, tuneKey } = useStore();
+
+  const key = tuneKey.label;
+  const id = activeMusicContext.id;
+
+  const descriptions = {
+    tonic: (
+      <>
+        In the key of <span>{key}</span>, a sense of resolution may be provided
+        by the <span>{majorRootName}&nbsp;Major</span> or{" "}
+        <span>{minorRootName}&nbsp;Minor</span> chords.{" "}
+        <span>{activeShapeName} Set</span> will expose the following colors of
+        each:
+      </>
+    ),
+
+    subdominant: (
+      <>
+        In the key of <span>{key}</span>, a sense of motion may be provided by
+        the <span>{majorRootName}&nbsp;Major</span> or{" "}
+        <span>{minorRootName}&nbsp;Minor</span> chords.{" "}
+        <span>{activeShapeName} Set</span> will expose the following colors of
+        each:
+      </>
+    ),
+
+    dominant: (
+      <>
+        In the key of <span>{key}</span>, a sense of tension to{" "}
+        <span>{key.split("/")[0]} Major</span> chord may be provided by the{" "}
+        <span>{majorRootName}&nbsp;Major</span> chord.{" "}
+        <span>{activeShapeName}</span> Set will expose the following colors of
+        it:
+      </>
+    ),
+
+    phDominant: (
+      <>
+        In the key of <span>{key}</span>, a sense of tension to{" "}
+        <span>{key.split("/")[1]?.replace("m", "")} Minor</span> chord may be
+        provided by the <span>{majorRootName}&nbsp;Major</span> chord.{" "}
+        <span>{activeShapeName} Set</span> will expose the following colors of
+        it:
+      </>
+    ),
+  };
+
+  const description =
+    id === "tonics"
+      ? descriptions.tonic
+      : id === "subdominants"
+      ? descriptions.subdominant
+      : id === "dominant"
+      ? descriptions.dominant
+      : id === "dominant-ph"
+      ? descriptions.phDominant
+      : "";
+
+  return description;
+};
+
+export default useDescription;
