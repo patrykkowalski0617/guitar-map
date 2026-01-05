987a91eab9e3e445ce1a85de015d1da3638de684
Patryk Kowalski <patrykkowalski0617@gmail.com>
2026-01-05 09:40:42 +0100

    enharmonic transofrm fix

--- src/utils/enharmonicTransform.js
+++ src/utils/enharmonicTransform.js
@@ -7,7 +7,14 @@ const enharmonicTransform = (text) => {
     "C#": "Db",
   };
 
-  const pattern = new RegExp(Object.keys(map).join("|"), "g");
+  // Sortujemy klucze od najdłuższych (np. "A#m" przed "A#")
+  // Dodajemy (?![a-zA-Z#]) - to upewnia się, że po dopasowaniu
+  // nie ma kolejnej litery (jak 'm') ani kolejnego płotu '#'
+  const sortedKeys = Object.keys(map).sort((a, b) => b.length - a.length);
+  const pattern = new RegExp(
+    sortedKeys.map((k) => `${k.replace("#", "\\#")}(?![m])`).join("|"),
+    "g"
+  );
 
   return text.replace(pattern, (matched) => map[matched]);
 };
