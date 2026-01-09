a4040695a291c57e8a21242b9a0baa85d1a66cce
Patryk Kowalski <patrykkowalski0617@gmail.com>
2026-01-09 11:32:05 +0100

    u p

--- src/data/userProgress.js
+++ src/data/userProgress.js
@@ -1,6 +1,26 @@
 const userProgress = {
-  M7: ["M7_5", "M7_6", "M7_11", "M7_17"],
-  dominant: ["dominant_15", "dominant_14", "dominant_20", "dominant_38"],
-  dim7: ["dim7_15", "dim7_14", "dim7_20", "dim7_26"],
+  M7: [
+    "M7_5",
+    "M7_6",
+    "M7_11",
+    "M7_17",
+    //
+    "M7_15",
+  ],
+  dominant: [
+    "dominant_15",
+    "dominant_14",
+    "dominant_20",
+    "dominant_38",
+    //
+  ],
+  dim7: [
+    "dim7_15",
+    "dim7_14",
+    "dim7_20",
+    "dim7_26",
+    //
+    "dim7_18",
+  ],
 };
 export default userProgress;
